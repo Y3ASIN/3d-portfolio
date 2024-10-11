e9db0ef7264d6ff6d51e21bd65640655d8ca43da
Y3ASIN <yeasinarafat.wa@gmail.com>
2024-10-11 19:14:08 +0600

    Fix: Sentry, and update computer glt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,26 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-import { styles } from "../styles"
-import { ComputersCanvas } from "./canvas"
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] mx-w-7xl flex flex-row mx-auto items-start gap-5`} >
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] mx-w-7xl flex flex-row mx-auto items-start gap-5`}
+      >
         <div className="flex flex-col justify-center items-center mt-5">
-            <div className="h-5 w-5 rounded-full bg-[#915eff]" />
-            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="h-5 w-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I&apos;m <span className="text-[#915eff]">Yeasin</span> </h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I&apos;m <span className="text-[#915eff]">Yeasin</span>{" "}
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-             I develop 3D visuals, user <br className="sm:block hidden"/> interfaces and web applications.
+            I develop 3D visuals, user <br className="sm:block hidden" />{" "}
+            interfaces and web applications.
           </p>
         </div>
       </div>
@@ -25,18 +30,20 @@ const Hero = () => {
       <div className="absolute xs:bottom-2 bottom-14 w-full flex justify-center items-center ">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-              <motion.div animate={{y:[0,24,0]}} transition={{
-                duration:1.5,
-                repeat:Infinity,
-                repeatType:'loop'
-              }} 
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
               className="w-3 h-3 rounded-full bg-secondary mb-1"
-              />
+            />
           </div>
         </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+
+Sentry.init({
+  dsn: "https://bae767bcfe60d1158ee3957bcdffd408@o4508047440936960.ingest.us.sentry.io/4508099788931072",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
